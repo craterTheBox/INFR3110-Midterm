8536f5dad25554d5931644376fdca77abe18fd6c
Carter Menary <carter.menary@gmail.com>
2020-10-08 00:50:44 -0400

    Made the maze and hazards

---
+++ Assets/_Scripts/BouncingPlatforms.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using UnityEngine;
+
+public class BouncingPlatforms : MonoBehaviour
+{
+    //Like the ObjSpin script, this is solely to make platforms move between two points
+    [SerializeField] float speed;
+    [SerializeField] GameObject obj1;
+    [SerializeField] GameObject obj2;
+
+    bool reversed = false;
+    float startTime;
+    float dist, distA, distC; //just needed for Lerp to work properly
+
+
+    void Start()
+    {
+        transform.position = obj1.transform.position; //Starts it at obj1
+        dist = Vector3.Distance(obj1.transform.position, obj2.transform.position);
+    }
+
+    void Update()
+    {
+        distA = ((Time.time - startTime) * speed) / dist;
+
+        //Lerping time
+        if (!reversed)
+        {
+            transform.position = Vector3.Lerp(obj1.transform.position, obj2.transform.position, distA);
+        }
+        else if (reversed)
+        {
+            transform.position = Vector3.Lerp(obj2.transform.position, obj1.transform.position, distA);
+        }
+
+        if (transform.position == obj1.transform.position)
+        {
+            reversed = false;
+            startTime = Time.time;
+            distA = (Time.time - startTime) / dist;
+        }
+        else if (transform.position == obj2.transform.position)
+        {
+            reversed = true;
+            startTime = Time.time;
+            distA = (Time.time - startTime) / dist;
+        }
+    }
+}
---
+++ Assets/_Scripts/ObjSpin.cs
@@ -0,0 +1,16 @@
+﻿using FMOD;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ObjSpin : MonoBehaviour
+{
+    //Pretty simple: you attach this to something and it spins
+    [SerializeField] float speed;
+
+    void Update()
+    {
+        transform.Rotate(-(Vector3.up * (speed * Time.deltaTime)));
+    }
+}
